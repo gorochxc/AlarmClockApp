fc65cefb79d1b278f06a381cdfdae347cf54affc
gorochxc <128721383+gorochxc@users.noreply.github.com>
2023-10-18 13:36:43 +0700

    ss
    ss

--- AlarmClockApp/Forms/SettingsForm.Designer.cs
+++ AlarmClockApp/Forms/SettingsForm.Designer.cs
@@ -4,15 +4,10 @@ namespace AlarmClockApp.Forms
 {
     partial class SettingsForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
